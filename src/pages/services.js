3321d0f2d9748deccde4c35bea34b2711d2fb376
vijay kumar <vijay.kumar@emvigotech.com>
2022-02-08 01:11:43 +0530

    Update Service Page Box Settings

--- src/pages/services.js
+++ src/pages/services.js
@@ -16,7 +16,8 @@ function Services() {
                 <i class="fas fa-mobile-alt"></i>
                 <h4 class="text-capitalize">Fully Responsive</h4>
                 <p>
-                  The websites we develop are fully responsive and adapt to devices automatically.
+                  The websites we develop are fully responsive and adapt to
+                  devices automatically.
                 </p>
               </div>
             </div>
@@ -25,7 +26,8 @@ function Services() {
                 <i class="far fa-user"></i>
                 <h4 class="text-capitalize">User Friendly</h4>
                 <p>
-                  We create user friendly sites so that it adapts to the user's usability.
+                  We create user friendly sites so that it adapts to the user's
+                  usability.
                 </p>
               </div>
             </div>
@@ -34,7 +36,8 @@ function Services() {
                 <i class="far fa-file-alt"></i>
                 <h4 class="text-capitalize">Well Documented</h4>
                 <p>
-                  Our sites are well documented so that we develop them based on user's requirements.
+                  Our sites are well documented so that we develop them based on
+                  user's requirements.
                 </p>
               </div>
             </div>
@@ -43,18 +46,20 @@ function Services() {
                 <i class="fas fa-key"></i>
                 <h4 class="text-capitalize">Easy &amp; Customizable</h4>
                 <p>
-                  We customize your sites based on your needs and preferences, which can be flexibile enough.
+                  We customize your sites based on your needs and preferences,
+                  which can be flexibile enough.
                 </p>
               </div>
             </div>
           </div>
-          <div class="row">
+          <div class="row" style={{justifyContent: "center"}}>
             <div class="col-md-6 col-lg-3 margin-b-30">
               <div class="feature-box-center text-center">
                 <i class="fas fa-globe"></i>
                 <h4 class="text-capitalize">Cross Browser</h4>
                 <p>
-                  Our sites support all the latest browsers with ease and give you a nice experience in viewing.
+                  Our sites support all the latest browsers with ease and give
+                  you a nice experience in viewing.
                 </p>
               </div>
             </div>
@@ -63,7 +68,8 @@ function Services() {
                 <i class="fab fa-bootstrap"></i>
                 <h4 class="text-capitalize">Based on bootstrap</h4>
                 <p>
-                  Our Bootstrap based websites make them very responsive and user friendly and also give you a smooth usability.
+                  Our Bootstrap based websites make them very responsive and
+                  user friendly and also give you a smooth usability.
                 </p>
               </div>
             </div>
@@ -83,9 +89,11 @@ function Services() {
                 />
                 <h3 class="item-title">Web & App Development</h3>
                 <div class="item-desc">
-                  We provide services in web & app development. We use all the latest 
-                  libraries, technologies, and custom development according to the user requirement.
-                  We are working with Scripting language and Core Programming languauges for better performance.
+                  We provide services in web & app development. We use all the
+                  latest libraries, technologies, and custom development
+                  according to the user requirement. We are working with
+                  Scripting language and Core Programming languauges for better
+                  performance.
                 </div>
               </div>
             </div>
@@ -98,8 +106,10 @@ function Services() {
                 />
                 <h3 class="item-title">Increase Productivity</h3>
                 <div class="item-desc">
-                  We initiate working according to the SCRUM, where we can discuss our everyday plan and schedule.
-                  It will automatically increase productivity while getting daily status and performance rate.
+                  We initiate working according to the SCRUM, where we can
+                  discuss our everyday plan and schedule. It will automatically
+                  increase productivity while getting daily status and
+                  performance rate.
                 </div>
               </div>
             </div>
@@ -112,9 +122,11 @@ function Services() {
                 />
                 <h3 class="item-title">Better Collaboration</h3>
                 <div class="item-desc">
-                 We also work on better collaboration with businesses where we will help them grow exponentially 
-                 using our custom build websites. Also we will work and give the businesses support in SEO & Digital 
-                 Marketing with their brand new websites where they can have a very good start.
+                  We also work on better collaboration with businesses where we
+                  will help them grow exponentially using our custom build
+                  websites. Also we will work and give the businesses support in
+                  SEO & Digital Marketing with their brand new websites where
+                  they can have a very good start.
                 </div>
               </div>
             </div>
